a38c1aec8f2b994a0d94e847cda87ed6528f2dca
Bill Wendling <morbo@google.com>
2026-01-14 15:30:27 -0800

    fixup! More work.

--- llvm/lib/CodeGen/InlineAsmPrepare.cpp
+++ llvm/lib/CodeGen/InlineAsmPrepare.cpp
@@ -20,6 +20,8 @@
 #include "llvm/IR/Module.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/Pass.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/DerivedTypes.h"
 #include <sstream>
 
 using namespace llvm;
@@ -60,7 +62,6 @@ static SmallVector<CallBase *, 4> findInlineAsms(Function &F) {
       if (!CB || !CB->isInlineAsm())
         return;
       InlineAsms.push_back(CB);
-      CB->dump();
     });
   });
 
@@ -68,8 +69,15 @@ static SmallVector<CallBase *, 4> findInlineAsms(Function &F) {
 }
 
 static bool isRegMemConstraint(StringRef Constraint) {
-  return Constraint.size() == 2 && is_contained(Constraint, "r") &&
-    is_contained(Constraint, "m");
+  // Strip prefixes like '=', '+', '*'
+  while (!Constraint.empty() && (Constraint[0] == '=' ||
+                                Constraint[0] == '+' ||
+                                Constraint[0] == '*')) {
+    Constraint = Constraint.substr(1);
+  }
+  // Check for "rm" now.
+  bool Result = Constraint.size() == 2 && Constraint.contains('r') && Constraint.contains('m');
+  return Result;
 }
 
 // Convert instances of the "rm" constraints into "m".
@@ -110,50 +118,128 @@ static std::string convertConstraintsToMemory(StringRef ConstraintStr) {
 
 bool InlineAsmPrepare::runOnFunction(Function &F) {
   // Only process "rm" on x86 platforms.
-  if (F.getParent()->getTargetTriple().getArch() != Triple::x86 &&
-      F.getParent()->getTargetTriple().getArch() != Triple::x86_64)
+  if (!F.getParent()->getTargetTriple().isX86())
     return false;
 
   SmallVector<CallBase *, 4> IAs = findInlineAsms(F);
   if (IAs.empty())
     return false;
 
+  // errs() << "OLD: "; F.dump();
+
   bool Changed = false;
   for (CallBase *CB : IAs) {
     InlineAsm *IA = cast<InlineAsm>(CB->getCalledOperand());
-    InlineAsm::ConstraintInfoVector Constraints = IA->ParseConstraints();
+    const InlineAsm::ConstraintInfoVector &Constraints = IA->ParseConstraints();
 
-#if 0
-    for (auto &C : Constraints) {
-      errs() << "Type: " << C.Type << "\n"
-             << "Early Clobber: " << C.isEarlyClobber << "\n"
-             << "Matching Input: " << C.MatchingInput << "\n"
-             << "Commutative: " << C.isCommutative << "\n"
-             << "Indirect: " << C.isIndirect << "\n"
-             << "Multiple Alternative: " << C.isMultipleAlternative << "\n";
+    bool HasRM = false;
+    StringRef OriginalConstraintStr = IA->getConstraintString();
+    auto StrI = OriginalConstraintStr.begin(), StrE = OriginalConstraintStr.end();
 
-      errs() << "Constraint Codes:\n";
-      for (auto Code : C.Codes)
-        errs() << "\t" << Code << "\n";
-
-      for (auto &CI : C.multipleAlternatives) {
-        errs() << "Multiple Alt Constraints:\n";
-        for (auto Code : CI.Codes)
-          errs() << "\t" << Code << "\n";
+    while (StrI != StrE) {
+      if (*StrI == '=') {
+        ++StrI;
+      }
+      if (*StrI == '*') {
+        ++StrI;
+      }
+      if (*StrI == '+') {
+        ++StrI;
       }
 
-      if (C.Codes.size() == 2 && is_contained(C.Codes, "r") &&
-          is_contained(C.Codes, "m")) {
-        errs() << "Needs processing.\n";
+      auto Comma = std::find(StrI, StrE, ',');
+      StringRef Sub(StrI, std::distance(StrI, Comma));
+      if (isRegMemConstraint(Sub)) {
+        HasRM = true;
+        break;
       }
+
+      if (Comma == StrE)
+        break;
+
+      StrI = Comma + 1;
     }
-#endif
+
+    if (!HasRM)
+      continue;
 
     std::string NewConstraintStr =
         convertConstraintsToMemory(IA->getConstraintString());
+    if (NewConstraintStr == IA->getConstraintString())
+      continue;
 
-    errs() << "OLD CONSTRAINT STR: " << IA->getConstraintString() << "\n";
-    errs() << "NEW CONSTRAINT STR: " << NewConstraintStr << "\n";
+    Changed = true;
+
+    IRBuilder<> Builder(CB);
+    IRBuilder<> EntryBuilder(&F.getEntryBlock(), F.getEntryBlock().begin());
+
+    unsigned ArgNo = 0;
+    unsigned OutputIdx = 0;
+    for (const auto &C : Constraints) {
+      if (!C.Codes.empty() && isRegMemConstraint(C.Codes[0])) {
+        Type *SlotTy = nullptr;
+        if (C.Type == InlineAsm::isOutput && !C.hasMatchingInput()) {
+          // Output-only
+          Type *RetTy = CB->getType();
+          if (StructType *ST = dyn_cast<StructType>(RetTy)) {
+            SlotTy = ST->getElementType(OutputIdx);
+          } else {
+            SlotTy = RetTy;
+          }
+        } else {
+          // Input or Read-Write
+          SlotTy = CB->getArgOperand(ArgNo)->getType();
+        }
+
+        AllocaInst *Slot = EntryBuilder.CreateAlloca(SlotTy, nullptr, "asm_mem");
+
+        if (C.Type == InlineAsm::isInput ||
+            (C.Type == InlineAsm::isOutput && C.hasMatchingInput())) {
+          // Input part of input-only or read-write
+          Builder.CreateStore(CB->getArgOperand(ArgNo), Slot);
+        }
+
+        if (C.Type == InlineAsm::isOutput) {
+          // Output part of output-only or read-write
+          Instruction *InsertPt = CB->getNextNode();
+          if (InvokeInst *II = dyn_cast<InvokeInst>(CB))
+            InsertPt = &II->getNormalDest()->front();
+
+          if (InsertPt) {
+            IRBuilder<> PostBuilder(InsertPt);
+            PostBuilder.CreateLoad(SlotTy, Slot, "asm_load");
+          } else if (!CB->isTerminator()) {
+            // This case should not be hit if we get NextNode
+          } else {
+            // Terminator, but not invoke, or invoke without normal dest.
+            // Insert load before the call. This is not ideal, but it's just
+            // for dumping IR.
+            Builder.CreateLoad(SlotTy, Slot, "asm_load");
+          }
+        }
+      }
+
+      if (C.Type == InlineAsm::isOutput || C.Type == InlineAsm::isInput) {
+        ArgNo++;
+      }
+
+      if (C.Type == InlineAsm::isOutput && !C.hasMatchingInput()) {
+        OutputIdx++;
+      }
+    }
+
+    auto *NewIA = InlineAsm::get(
+        IA->getFunctionType(), IA->getAsmString(), NewConstraintStr,
+        IA->hasSideEffects(), IA->isAlignStack(), IA->getDialect(),
+        IA->canThrow());
+    errs() << "OLD: ";
+    IA->dump();
+    errs() << "NEW: ";
+    NewIA->dump();
+  }
+
+  if (Changed) {
+    errs() << "NEW: "; F.dump();
   }
 
   return Changed;
